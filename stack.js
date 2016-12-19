92ba9b5687b68e7ec648454b90f1bdaa84dfcce9
brandon <mn.cres@gmail.com>
2016-12-19 11:08:30 -0600

    more stack

--- stack.js
+++ stack.js
@@ -26,25 +26,36 @@ Stack.prototype.size = function() {
     return length;
 }
 
-var myMenu = new Stack();
-myMenu.push('pizza');
-myMenu.push('steak');
+// var myMenu = new Stack();
+// myMenu.push('pizza');
+// myMenu.push('steak');
 
 
 // With an object as storage
 
-var Stack1 = function() {
+var Stack1 = function(capacity) {
+    this.capacity = capacity || Infinity;
     this.storage = {};
+    this.count = 0;
 }
 
 Stack1.prototype.push = function(val) {
-    // take val and add it to this.storage object
-    // return number of keys in this.storage
+    if(this.count < this.capacity) {
+        // take val and add it to this.storage object
+        this.storage[this.count++] = val;
+        // return number of keys in this.storage
+        return this.count;
+    }
+    return "Already at max capacity";
 }
 
 Stack1.prototype.pop = function() {
-    // remove most recent item from this.storage object
-    // return removed item
+    var value = this.storage[this.count - 1];
+    delete this.storage[value];
+    if(this.count < 0) {
+        this.count = 0;
+    }
+    return value;
 }
 
 Stack1.prototype.peek = function() {
@@ -55,3 +66,10 @@ Stack1.prototype.count = function() {
     // return number of elements in stack
 }
 
+var stack = new Stack1(5);
+
+console.log(stack.push('hello'));
+console.log(stack.push('yes'));
+console.log(stack.push('dude'));
+console.log(stack.pop());
+//console.log(stack);
