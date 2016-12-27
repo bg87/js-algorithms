0bb2f6a146757ac26ce29215231d3725ce70df8d
brandon <mn.cres@gmail.com>
2016-12-27 10:58:30 -0600

    finish recursive-functions

--- recursive-functions.js
+++ recursive-functions.js
@@ -11,11 +11,53 @@ function loopNTimes(n) {
 
 // console.log(loopNTimes(10));
 
-function factorial(int) {
-    if(int <= 1) {
-        return int;
-    }
-    return int * factorial(int - 1);
+function factorial(n) {
+    if(n <= 1) return 1;
+    return n * factorial(n - 1);
 }
 
-console.log(factorial(5));
+// console.log(factorial(5));
+
+function exponent(base, expo) {
+    if(expo === 0) {
+        return 1;
+    }
+    return base * exponent(base, expo - 1);
+}
+
+// console.log(exponent(2, 3));
+
+function countRecursive(start, end) {
+    function recurse(start) {
+        console.log(start);
+        if(start < end){
+            recurse(start + 1);
+        }
+    }
+    recurse(start);
+}
+
+// console.log(countRecursive(5, 10));
+
+
+function reverse(arr) {
+   var reversedArr = [];
+   function addItems() {
+       if(arr.length > 0){
+          reversedArr.push(arr.pop());
+          addItems;
+       }
+   }
+   addItems();
+   return reversedArr;
+}
+
+console.log(reverse([1,2,3]));
+
+function fibonacci(n) {
+    if(n <= 2) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+// console.log(fibonacci(20));
+
